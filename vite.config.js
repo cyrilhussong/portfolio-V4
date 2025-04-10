3218a177fe0bc0dd24dd4db98ace64b8c92b54cd
cyrilhussong <cyrilhussong347@gmail.com>
2025-04-11 01:23:56 +0200

    fix: ajout base pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-V4/', // ← ajoute cette ligne ici
   plugins: [
     vue(),
     vueDevTools(),
